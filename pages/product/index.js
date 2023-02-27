33d7ed292eaf40ac64bcfe40ff54a72d49313538
Sprof22 <richmondelaigwu@gmail.com>
2023-02-27 09:49:06 +0100

    you cant use anchor tag inside link element

--- pages/product/index.js
+++ pages/product/index.js
@@ -5,7 +5,7 @@ const ProductList = ({productID = 100}) => {
   return (
     <div>
     <Link href={'/'}>Go Home</Link>
-        <Link href='/product/1'>Product 1</Link>
+        <Link href='/product/1'><h2>Product 1</h2></Link>
         <Link href='/product/2'>Product 2</Link>
         <Link href='/product/3'>Product 3</Link>
         <Link href={`/product/${productID}`}>Product {productID}</Link>
