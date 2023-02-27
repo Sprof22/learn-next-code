24bce50d2af8e1db1a3bb37b914a9e77e4aa23dc
Sprof22 <richmondelaigwu@gmail.com>
2023-02-27 10:11:57 +0100

    replace

--- pages/product/index.js
+++ pages/product/index.js
@@ -7,7 +7,7 @@ const ProductList = ({productID = 100}) => {
     <Link href={'/'}>Go Home</Link>
         <Link href='/product/1'><h2>Product 1</h2></Link>
         <Link href='/product/2'>Product 2</Link>
-        <Link href='/product/3'>Product 3</Link>
+        <Link href='/product/3' replace>Product 3</Link>
         <Link href={`/product/${productID}`}>Product {productID}</Link>
         
     </div>
