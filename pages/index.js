7eb474f2e5a9acb8140ad2e058e2f8d31e7d513b
Sprof22 <richmondelaigwu@gmail.com>
2023-02-28 10:48:59 +0100

    push

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 function Home() {
+  const router = useRouter()
+  const handleClick = () => {
+    console.log('yeep I have been clicked');
+    router.push('/product')
+  }
   return (
     <div>
       <h1>Home Page</h1>
       <Link href="/blog">Blog Page</Link>
       <Link href="/product">Product Page</Link>
+      <button onClick={handleClick}>Click Me!</button>
     </div>
   )
 }
