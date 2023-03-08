3d8b28b53132177fb5c1c2fdac8ce156758d44ac
Sprof22 <richmondelaigwu@gmail.com>
2023-03-08 15:49:05 +0100

    push

--- challenge/pages/index.js
+++ challenge/pages/index.js
@@ -3,13 +3,36 @@ import { Inter } from "next/font/google";
 import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
+const details = [
+  {
+    img: "./pic.svg",
+    header: "The Virtual Event Production Kit",
+    text: "Want templates built by event leaders for event leaders? In this kit, you’ll get seven templates and a glossary of terms ...",
+  },
+  {
+    img: "./pic.svg",
+    header: "The Virtual Event Production Kit",
+    text: "Want templates built by event leaders for event leaders? In this kit, you’ll get seven templates and a glossary of terms ...",
+  },
+  {
+    img: "./pic.svg",
+    header: "The Virtual Event Production Kit",
+    text: "Want templates built by event leaders for event leaders? In this kit, you’ll get seven templates and a glossary of terms ...",
+  }
+];
+
 const MainContainer = styled.div`
   display: flex;
-  grid-gap: 50px;
+  grid-gap: 19px;
   max-width: 1440px;
-  margin: 50px;
+  margin: auto;
+  margin-top: 100px;
   justify-content: center;
-  align-items: center;
+  align-items: center; 
+
+  @media (max-width: 992px) {
+    flex-direction: column;
+  }
 `;
 
 const ItemCard = styled.div`
@@ -17,6 +40,7 @@ const ItemCard = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: stretch;
+  width: 420px; n 
   height: 525px;
   margin-bottom: 30px;
   border-radius: 15px;
@@ -24,6 +48,26 @@ const ItemCard = styled.div`
     font-size: 18px;
     padding: 29px;
   }
+
+  > img {
+    border-radius: 25px;
+  }
+
+  > div {
+    padding: 30px;
+    
+  }
+
+  > div {
+     
+      &:nth-of-type(2) {
+        display: flex;
+        grid-gap: 20px;
+        align-items: center;
+        justify-content: start;
+        width: 80%
+      }
+    }
 
   >&: nth-of-type(1) img {
     width: 100%;
@@ -34,35 +78,20 @@ const ItemCard = styled.div`
 export default function Home() {
   return (
     <MainContainer className={inter.className}>
-      <ItemCard className="item-card">
-        <img src="./pic.svg" alt="" />
-        <h3>The Virtual Event Production Kit</h3>
-        <p>
-          Want templates built by event leaders for event leaders? In this kit,
-          you’ll get seven templates and a glossary of terms ...
-        </p>
+   {details.map(detail => (
+    <ItemCard className="item-card">
+        <img src={detail.img} alt="" />
+        <div>
+          <h3>{detail.header}</h3>
+          <p>{detail.text}</p>
+        </div>
+        <div>
         <img src="./arrow-circle.svg" />
+        <Link href="/">Learn More</Link>
+        </div>
+        
       </ItemCard>
-      <ItemCard className="item-card">
-        <img src="./pic.svg" alt="" />
-        <h3>The Virtual Event Production Kit</h3>
-        <p>
-          Want templates built by event leaders for event leaders? In this kit,
-          you’ll get seven templates and a glossary of terms ...
-        </p>
-        <img src="./arrow-circle.svg" />
-      </ItemCard>
-      <ItemCard className="item-card">
-        <img src="./pic.svg" alt="" />
-        <h3>The Virtual Event Production Kit</h3>
-        <p>
-          Want templates built by event leaders for event leaders? In this kit,
-          you’ll get seven templates and a glossary of terms ...
-        </p>
-        <Link href="/learnMore">
-          <img src="./arrow-circle.svg" />
-        </Link>
-      </ItemCard>
+   ))}
     </MainContainer>
   );
 }
