106ec80647a1985070d33062f832bccb786dea20
Sprof22 <richmondelaigwu@gmail.com>
2023-03-21 13:33:08 +0100

    getPokemon called

--- next-rendering/pages/index.js
+++ next-rendering/pages/index.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [pokeman, setPokeman] = useState([])
   useEffect(() => {
-    
-  
-   
+    async function getPokemon(){
+      let res = await fetch ('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json')
+      res = await res.json();
+console.log(res);
+       setPokeman([])
+    }  
+    getPokemon() 
   }, [])
   
   return (
@@ -17,6 +22,9 @@ export default function Home() {
       <Head>
         <title>The Pokeman List</title>
       </Head>
+      <div>
+        {JSON.stringify(pokeman)}
+      </div>
         
     </>
   )
